2edcaa75e3712689f31716142f9e66d96011f665
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2022-11-08 15:28:21 -0300

    correção de erros

--- compara.js
+++ compara.js
@@ -6,14 +6,10 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-    // const mensagem = 'Bem-vindo ao Alura Tube!'
     const estilosDaHomePage = {
         // backgroundColor: "red" 
     };
-
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
-
-    // console.log(config.playlists);
 
     return (
         <>
@@ -22,14 +18,14 @@ function HomePage() {
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
+                // backgroundColor: "red",
             }}>
-                {/* {mensagem} */}
+                {/* Prop Drilling */}
                 <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
                 <Timeline searchValue={valorDoFiltro} playlists={config.playlists}>
                     Conteúdo
                 </Timeline>
-                <Favoritos />
             </div>
         </>
     );
@@ -45,63 +41,48 @@ export default HomePage
 //     )
 // }
 
-const StyledHeader = styled.div`
-img{
-    width:80px;
-    height:80px;
-    border-radius:50%;
-    margin-top: 10px;
-}
-.user-info{
-    margin-top: 50px;
-    display:flex;
-    align-items: center;
-    width: 100%;
-    padding: 16px 32px;
-    gap: 16px;
-}
-.banner{
-width: 1512px;
-height: 230px;
-margin-top: 50px;
-margin-bottom: -75px;
-position: relative;
-left:0px;
-border-radius: 0%;
-background-color: blue;
-background-image: url(${({ bg }) => bg});
-height: 230px;
 
-}
+const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
 `;
 const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${({ bg }) => bg});
     /* background-image: url(${config.bg}); */
+    height: 230px;
 `;
-
 function Header() {
     return (
         <StyledHeader>
-            {/* banner             */}
-            {/* <StyledBanner bg={config.banner}/> */}
-             <img className="banner" src={config.banner} /> 
-            {/* informações de usuário */}
+            <StyledBanner bg={config.bg} />
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
                     <h2>
                         {config.name}
-                        </h2>
+                    </h2>
                     <p>
                         {config.job}
-                        </p>
+                    </p>
                 </div>
             </section>
         </StyledHeader>
     )
 }
-// ou props
-function Timeline({searchValue, ...propriedades}) {
-    // console.log("Dentro do componente", props.playlists);
+
+function Timeline({ searchValue, ...propriedades }) {
+    // console.log("Dentro do componente", propriedades.playlists);
     const playlistNames = Object.keys(propriedades.playlists);
     // Statement
     // Retorno por expressão
@@ -109,13 +90,12 @@ function Timeline({searchValue, ...propriedades}) {
         <StyledTimeline>
             {playlistNames.map((playlistName) => {
                 const videos = propriedades.playlists[playlistName];
-                // console.log(playlistsName);
+                // console.log(playlistName);
                 // console.log(videos);
                 return (
                     <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {/* filter é um comando da array do JS */}
                             {videos
                                 .filter((video) => {
                                     const titleNormalized = video.title.toLowerCase();
@@ -136,12 +116,6 @@ function Timeline({searchValue, ...propriedades}) {
                     </section>
                 )
             })}
-            {/* Frontend
-            Jogos */}
-            {/* </div> */}
         </StyledTimeline>
     )
-}
-function Favoritos() {
-    <h2>Favoritos AluraTubes</h2>
 }
